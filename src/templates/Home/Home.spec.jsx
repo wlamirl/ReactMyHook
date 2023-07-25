c3a97cc6ddd9c1392db80e38aeb9591673100e9d
wlamirl <wlamirl@yahoo.com.br>
2023-07-25 17:59:14 -0300

    acertos segundo hook

---
+++ src/templates/Home/Home.spec.jsx
@@ -0,0 +1,22 @@
+/* import { render, screen } from "@testing-library/react";
+import App from ".";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+}); */
+
+describe('<Home />', () => {
+  it('is a dummy test one', () => {
+    expect(1).toBe(1);
+  });
+
+  /*   it("test two", () => {
+    expect(1).toBe(1);
+  });
+
+  it("test three", () => {
+    expect(1).toBe(1);
+  }); */
+});
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -106,3 +106,4 @@ export const Home = () => {
 
   return <h1>Oi</h1>;
 };
+export default Home;
