6681e5a1d931c922f313d3b0e786104d73d819e8
wlamirl <wlamirl@yahoo.com.br>
2023-07-25 18:22:02 -0300

    acertos nos arquivos de configuração

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['react'],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
